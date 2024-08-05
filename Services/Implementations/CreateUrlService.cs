b7ad1e0c9970ded62752923553fe82ab16019775
b_e_sa <joaospereira963@gmail.com>
2024-08-05 02:18:29 -0300

    add missing namespace

--- Services/Implementations/CreateUrlService.cs
+++ Services/Implementations/CreateUrlService.cs
@@ -2,35 +2,40 @@ using Shortener.Data.Repositories;
 using Shortener.Models;
 using Shortener.Services.Models;
 
-class CreateUrlService(IUrlRepository urlRepository) : ICreateUrlService
+namespace Shortener.Services.Implementations
 {
-    private readonly IUrlRepository _urlRepository = urlRepository;
-
-    private static string ShortUrl()
+    class CreateUrlService(IUrlRepository urlRepository) : ICreateUrlService
     {
-        string shortUrl = "";
-        string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ0123456789";
+        private readonly IUrlRepository _urlRepository = urlRepository;
 
-        Random random = new();
-        for (int i = 0; i < 4; i++)
+        private static string ShortUrl()
         {
-            int index = random.Next(0, chars.Length);
-            shortUrl += chars[index];
+            string shortUrl = "";
+            string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ0123456789";
+
+            Random random = new();
+            for (int i = 0; i < 4; i++)
+            {
+                int index = random.Next(0, chars.Length);
+                shortUrl += chars[index];
+            }
+            return shortUrl;
         }
-        return shortUrl;
-    }
 
-    public async Task<Url> Execute(string url, string title)
-    {
-        var newUrl = new Url
+        public async Task<Url> Execute(string url, string title)
         {
-            OriginalUrl = url,
-            ShortUrl = ShortUrl(),
-            Title = title,
-            Visits = 0,
-            CreatedAt = DateTime.UtcNow
-        };
+            var newUrl = new Url
+            {
+                OriginalUrl = url,
+                ShortUrl = ShortUrl(),
+                Title = title,
+                Visits = 0,
+                CreatedAt = DateTime.UtcNow
+            };
 
-        return await _urlRepository.Create(newUrl);
+            await _urlRepository.Create(newUrl);
+
+            return newUrl;
+        }
     }
 }
