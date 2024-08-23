b9fd5f9a11637485207f9c4806109047961347f1
b_e_sa <joaospereira963@gmail.com>
2024-08-23 15:23:05 -0300

    add find url by id tests

---
+++ Tests/Urls/FindUrlByIdTests.cs
@@ -0,0 +1,59 @@
+using System.Net;
+using FluentAssertions;
+using Shortener.Controllers;
+using Shortener.Tests.Abstractions;
+using Xunit;
+
+namespace Shortener.Tests.Urls
+{
+    public class FindUrlByIdTests(FunctionalTestWebAppFactory factory) : BaseFunctionalTest(factory)
+    {
+        private readonly TestHelper helper = new();
+
+        [Fact]
+        public async Task Should_ReturnOk_WhenUrlExists()
+        {
+            // Arrange
+            var url = new UrlCreateRequest("New Url", "https://www.google.com");
+            var createdRes = await HttpClient.PostAsJsonAsync("http://localhost:5229", url);
+            var createdBody = await helper.DeserializeResponse<UrlCreateResponse>(createdRes);
+
+            // Act
+            var foundRes = await HttpClient.GetAsync($"http://localhost:5229/find/{createdBody.Data?.Id}");
+
+            // Assert
+            var foundBody = await helper.DeserializeResponse<UrlCreateResponse>(foundRes);
+            foundRes.StatusCode.Should().Be(HttpStatusCode.OK);
+            foundBody.Data?.Title.Should().Be("New Url");
+        }
+
+        [Fact]
+        public async Task Should_ReturnNotFound_WhenUrlDoesNotExists()
+        {
+            // Arrange
+            var id = "10";
+
+            // Act
+            var visitRes = await HttpClient.GetAsync($"http://localhost:5229/find/{id}");
+
+            // Assert
+            visitRes.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Should_ReturnBadRequest_WhenIdIsInvalid()
+        {
+            // Arrange
+            var id1 = "abc";
+            var id2 = "abcde";
+
+            // Act
+            var firstVisitRes = await HttpClient.GetAsync($"http://localhost:5229/find/{id1}");
+            var secondVisitRes = await HttpClient.GetAsync($"http://localhost:5229/find/{id2}");
+
+            // Assert
+            firstVisitRes.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            secondVisitRes.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
