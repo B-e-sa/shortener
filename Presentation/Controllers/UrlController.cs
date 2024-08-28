7383e8983d3a969639a382849f73d4ee477891a6
b_e_sa <joaospereira963@gmail.com>
2024-08-28 01:32:03 -0300

    remove unused code

--- Presentation/Controllers/UrlController.cs
+++ Presentation/Controllers/UrlController.cs
@@ -1,8 +1,6 @@
-using System.Text.RegularExpressions;
 using Mapster;
 using Microsoft.AspNetCore.Mvc;
-using Shortener.Application.Commands.Url;
-using Shortener.Controllers.ResponseHandlers.ErrorHandlers;
+using Shortener.Application.Commands.Url.CreateUrl;
 
 namespace Shortener.Presentation.Controllers
 {
@@ -19,24 +17,6 @@ namespace Shortener.Presentation.Controllers
             var urlId = await Sender.Send(command, cancellationToken);
 
             return CreatedAtAction(nameof(Handle), new { urlId }, urlId);
-            
-            if (
-                req == null
-                || string.IsNullOrEmpty(req.Title)
-                || string.IsNullOrEmpty(req.Url)
-            )
-                return BadRequest(new BadRequestHandler()
-                {
-                    Message = "The object must contain the Url and the respective title"
-                });
-
-            var urlReg = @"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)";
-
-            if (!Regex.Match(req.Url, urlReg).Success)
-                return BadRequest(new BadRequestHandler()
-                {
-                    Message = "Invalid Url format"
-                });
         }
     }
 }
