85c57dae969389dc37d8153923f7675663447baf
b_e_sa <joaospereira963@gmail.com>
2024-08-29 01:34:56 -0300

    add new url query routes

--- Presentation/Controllers/UrlController.cs
+++ Presentation/Controllers/UrlController.cs
@@ -1,13 +1,15 @@
 using Mapster;
 using Microsoft.AspNetCore.Mvc;
-using Shortener.Application.Commands.Url.CreateUrl;
+using Shortener.Application.Url.Commands.CreateUrl;
+using Shortener.Application.Url.Queries;
+using Shortener.Application.Url.Queries.FindUrlByShortUrl;
 
 namespace Shortener.Presentation.Controllers
 {
     public class UrlController() : ApiController
     {
         [HttpPost]
-        public async Task<IActionResult> Handle(
+        public async Task<IActionResult> CreateUrl(
             [FromBody] CreateUrlCommand req,
             CancellationToken cancellationToken
         )
@@ -16,7 +18,33 @@ namespace Shortener.Presentation.Controllers
 
             var urlId = await Sender.Send(command, cancellationToken);
 
-            return CreatedAtAction(nameof(Handle), new { urlId }, urlId);
+            return CreatedAtAction(nameof(CreateUrl), new { urlId }, urlId);
+        }
+
+        [HttpGet("find/{id}")]
+        public async Task<IActionResult> FindById(
+            int id,
+            CancellationToken cancellationToken
+        )
+        {
+            var query = new FindUrlByIdQuery(id);
+
+            var foundUrl = await Sender.Send(query, cancellationToken);
+
+            return Ok(foundUrl);
+        }
+
+        [HttpGet("{url}")]
+        public async Task<IActionResult> FindByShortUrl(
+            string url,
+            CancellationToken cancellationToken
+        )
+        {
+            var query = new FindUrlByShortUrlQuery(url);
+
+            var foundUrl = await Sender.Send(query, cancellationToken);
+
+            return Ok(foundUrl);
         }
     }
 }
