8ae30de5e532ddd790d1ea775f98d32ccb2aee31
b_e_sa <joaospereira963@gmail.com>
2024-08-31 16:02:51 -0300

    move get top urls from commands to queries

---
+++ Application/Url/Queries/GetTopUrls/GetTopUrls.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Shortener.Infrastructure;
+
+namespace Shortener.Application.Url.Queries.GetTopUrls
+{
+    public class GetTopUrlsCommandHandler(AppDbContext dbContext)
+    {
+        private readonly AppDbContext _dbContext = dbContext;
+
+        public async Task<List<Domain.Entities.Url>> Execute()
+        {
+            var oneWeekAgo = DateTime.UtcNow.AddDays(-7);
+            return await _dbContext.Urls
+                .Where(u => u.CreatedAt >= oneWeekAgo)
+                .OrderByDescending(u => u.Visits)
+                .Take(10)
+                .ToListAsync();
+        }
+    }
+}
