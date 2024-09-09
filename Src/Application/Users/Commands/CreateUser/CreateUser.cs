6edd63cde3cec68561d1e2dc5eb352260315e039
b_e_sa <joaospereira963@gmail.com>
2024-09-09 17:39:20 -0300

    add delete user command and implement providers on create user command

--- Src/Application/Users/Commands/CreateUser/CreateUser.cs
+++ Src/Application/Users/Commands/CreateUser/CreateUser.cs
@@ -1,35 +1,40 @@
 using MediatR;
 using Shortener.Application.Common.Interfaces;
+using Shortener.Application.Users.Abstractions;
 using Shortener.Domain.Entities;
 
 namespace Shortener.Application.Users.Commands.CreateUser;
 
-public record CreateUserCommand : IRequest<User>
+public record CreateUserCommand : IRequest<string>
 {
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
 }
 
-class CreateUserCommandHandler(IAppDbContext context) : IRequestHandler<CreateUserCommand, User>
+class CreateUserCommandHandler(
+    IAppDbContext context, 
+    IJwtProvider jwt, 
+    IEncryptionProvider encryption
+) : IRequestHandler<CreateUserCommand, string>
 {
     private readonly IAppDbContext _context = context;
+    private readonly IJwtProvider _jwt = jwt;
+    private readonly IEncryptionProvider _encryption = encryption;
 
-    public async Task<User> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    public async Task<string> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        string salt = BCrypt.Net.BCrypt.GenerateSalt(10);
-        string hash = BCrypt.Net.BCrypt.HashPassword(request.Password, salt);
-
         var newUser = new User
         {
             Email = request.Email,
             Username = request.Username,
-            Password = hash,
+            Password = _encryption.Hash(request.Password),
         };
+
 
         _context.Users.Add(newUser);
         await _context.SaveChangesAsync(cancellationToken);
 
-        return newUser;
+        return _jwt.Generate(newUser);
     }
 }
---
+++ Src/Application/Users/Commands/DeleteUser.cs/DeleteUser.cs
@@ -0,0 +1,24 @@
+using Ardalis.GuardClauses;
+using MediatR;
+using Shortener.Application.Common.Interfaces;
+
+namespace Shortener.Application.Users.Commands.DeleteUser;
+
+public record DeleteUserCommand(int Id) : IRequest;
+
+public class DeleteUserCommandHandler(IAppDbContext context) : IRequestHandler<DeleteUserCommand>
+{
+    private readonly IAppDbContext _context = context;
+
+    public async Task Handle(DeleteUserCommand req, CancellationToken cancellationToken)
+    {
+        var entity = await _context.Users
+            .FindAsync([req.Id], cancellationToken);
+
+        Guard.Against.NotFound(req.Id, entity);
+
+        _context.Users.Remove(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+};
