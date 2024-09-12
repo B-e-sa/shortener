48a0c96096a364c66b1a20577548da403aff2626
b_e_sa <joaospereira963@gmail.com>
2024-09-11 23:33:35 -0300

    refactir jwt provider class and made it public

--- Src/Infrastructure/Authentication/Jwt/JwtProvider.cs
+++ Src/Infrastructure/Authentication/Jwt/JwtProvider.cs
@@ -9,40 +9,35 @@ using Microsoft.Extensions.Options;
 
 namespace Shortener.Infrastructure.Authentication.Jwt
 {
-
-    internal sealed class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
+    public sealed class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
     {
         private readonly JwtOptions _options = options.Value;
 
         public string Generate(User user)
         {
-            if (string.IsNullOrEmpty(_options.Secret))
+            var claims = new Claim[]
             {
-                throw new ArgumentNullException(nameof(_options.Secret), "JWT Secret is not set.");
-            }
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-
-            var claims = new ClaimsIdentity(
-                [
-                    new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
-                ]);
-
-            var signinCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Secret)),
-                    SecurityAlgorithms.HmacSha256Signature);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = claims,
-                SigningCredentials = signinCredentials,
-                Issuer = _options.Issuer,
-                Audience = _options.Audience,
-                Expires = DateTime.UtcNow.AddDays(7),
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.Email, user.Email)
             };
 
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_options.Secret)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _options.Issuer,
+                _options.Audience,
+                claims,
+                null,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            string tokenValue = new JwtSecurityTokenHandler()
+                .WriteToken(token);
+
+            return tokenValue;
         }
     }
 }
