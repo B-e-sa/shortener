9b2bcb51e4df30cfd7b221fad9ba358e6af15f46
b_e_sa <joaospereira963@gmail.com>
2024-09-21 19:41:52 -0300

    add verify email testing

---
+++ Tests/Application.FunctionalTests/EmailVerifications/Commands/VerifyEmailTest.cs
@@ -0,0 +1,28 @@
+﻿using Shortener.Tests.Application.FunctionalTests.Users;
+using System.Net.Http.Headers;
+
+namespace Shortener.Tests.Application.FunctionalTests.EmailVerifications.Commands;
+
+public class VerifyEmailTests(FunctionalTestWebAppFactory factory) : BaseFunctionalTest(factory)
+{
+    private readonly UserHelper userHelper = new();
+    private readonly EmailVerificationHelper verificationHelper = new ();
+
+    [Fact]
+    public async Task Should_ReturnCreated_WhenRequestIsValid()
+    {
+        // Arrange
+        var user = userHelper.GenerateValidUser();
+        var userRes = await HttpClient.PostAsJsonAsync(userHelper.GetApiUrl(), user);
+        var userToken = await userRes.Content.ReadAsStringAsync();
+
+        // Act
+        var req = new HttpRequestMessage(HttpMethod.Post, verificationHelper.GetApiUrl());
+        req.Headers.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+
+        var createdEmailVerificationRes = await HttpClient.SendAsync(req);
+
+        // Assert
+        createdEmailVerificationRes.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+} 
