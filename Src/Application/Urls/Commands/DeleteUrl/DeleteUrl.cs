45a54b0c22551b8bc924b24b83445c2cac3676f1
b_e_sa <joaospereira963@gmail.com>
2024-09-24 15:09:31 -0300

    add authorization to url delete method

--- Src/Application/Urls/Commands/DeleteUrl/DeleteUrl.cs
+++ Src/Application/Urls/Commands/DeleteUrl/DeleteUrl.cs
@@ -4,20 +4,32 @@ using Shortener.Domain.Common.Exceptions.Urls;
 
 namespace Shortener.Application.Urls.Commands.DeleteUrl;
 
-public record DeleteUrlCommand(int Id) : IRequest;
+public record DeleteUrlCommand(int Id, string Token) : IRequest;
 
-public class DeleteUrlCommandHandler(IAppDbContext context) : IRequestHandler<DeleteUrlCommand>
+public class DeleteUrlCommandHandler(IAppDbContext context, ITokenService tokenService) : IRequestHandler<DeleteUrlCommand>
 {
     private readonly IAppDbContext _context = context;
+    private readonly ITokenService _tokenService = tokenService;
 
     public async Task Handle(DeleteUrlCommand req, CancellationToken cancellationToken)
     {
-        var entity = await _context.Urls
+        var payload = _tokenService.GetPayload(req.Token);
+
+        var foundUrl = await _context.Urls
             .FindAsync([req.Id], cancellationToken)
             ?? throw new UrlNotFoundException();
 
-        _context.Urls.Remove(entity);
+        if(foundUrl.UserId == null)
+        {
+            throw new UnauthorizedAccessException("Users cannot delete anonymously created URLs.");
+        }
 
+        if (payload.Id != foundUrl.UserId)
+        {
+            throw new UnauthorizedAccessException("Only the url owner can delete it.");
+        }
+
+        _context.Urls.Remove(foundUrl);
         await _context.SaveChangesAsync(cancellationToken);
     }
 };
