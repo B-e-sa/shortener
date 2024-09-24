d40a7e00f844710e6aaaad2205c00b21bf94eb75
b_e_sa <joaospereira963@gmail.com>
2024-09-24 00:07:39 -0300

    add token authentication route

---
+++ Src/Application/Users/Commands/AuthenticateToken/AuthenticateToken.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shortener.Application.Common.Interfaces;
+using Shortener.Application.Common.Models;
+using Shortener.Domain.Common.Exceptions.Users;
+
+namespace Shortener.Application.Users.Commands.AuthenticateToken;
+
+public record AuthenticateTokenCommand(string Token) : IRequest<UserDTO>;
+
+public class AuthenticateTokenCommandHandler(
+    IAppDbContext context,
+    ITokenService tokenService
+    ) : IRequestHandler<AuthenticateTokenCommand, UserDTO>
+{
+    private readonly IAppDbContext _context = context;
+    private readonly ITokenService _tokenService = tokenService;
+
+    public async Task<UserDTO> Handle(AuthenticateTokenCommand req, CancellationToken cancellationToken)
+    {
+        var payload = _tokenService.GetPayload(req.Token);
+
+        var foundUser = await _context.Users
+             .Where(u => u.Id == payload.Id)
+             .Select(u => new UserDTO
+             {
+                 Id = u.Id,
+                 UserName = u.Username,
+                 Email = u.Email,
+                 ConfirmedEmail = u.ConfirmedEmail
+             })
+             .FirstOrDefaultAsync(cancellationToken)
+             ?? throw new UserNotFoundException();
+
+        return foundUser;
+    }
+}
---
+++ Tests/Application.FunctionalTests/Users/Commands/AuthenticateTokenTest.cs
@@ -0,0 +1,29 @@
+using Shortener.Application.Common.Models;
+using System.Net.Http.Headers;
+
+namespace Shortener.Tests.Application.FunctionalTests.Users.Commands;
+
+public class AuthenticateTokenTests(FunctionalTestWebAppFactory factory) : BaseFunctionalTest(factory)
+{
+    private readonly UserHelper helper = new();
+
+    [Fact]
+    public async Task Should_ReturnAuthenticatedUser_WhenTokenIsValid()
+    {
+        // Arrange
+        var user = helper.GenerateValidUser();
+        var createdUserResponse = await HttpClient.PostAsJsonAsync(helper.GetApiUrl(), user);
+        var token = await createdUserResponse.Content.ReadAsStringAsync();
+
+        // Act
+        var req = new HttpRequestMessage(HttpMethod.Post, $"{helper.GetApiUrl()}/auth");
+        req.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var res = await HttpClient.SendAsync(req);
+
+        // Assert
+        res.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var resBody = await helper.DeserializeResponse<UserDTO>(res);
+        resBody.Email.Should().Be(user.Email);
+    }
+}
