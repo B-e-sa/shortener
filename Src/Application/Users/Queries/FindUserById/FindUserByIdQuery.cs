014f85a03649996b3873f0bc1048a123cc34e563
b_e_sa <joaospereira963@gmail.com>
2024-09-24 00:09:57 -0300

    switch find user by id query return from query response to userDTO

--- Src/Application/Users/Queries/FindUserById/FindUserByIdQuery.cs
+++ Src/Application/Users/Queries/FindUserById/FindUserByIdQuery.cs
@@ -1,39 +1,35 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Shortener.Application.Common.Interfaces;
+using Shortener.Application.Common.Models;
 using Shortener.Domain.Common.Exceptions.Users;
 
 namespace Shortener.Application.Users.Queries.FindUserById;
 
-public sealed record FindUserByIdQueryResponse(
-    int Id,
-    string Email,
-    ICollection<Url> Urls,
-    DateTime CreatedAt,
-    DateTime UpdatedAt);
-
-public sealed record FindUserByIdQuery(int Id) : IRequest<FindUserByIdQueryResponse>;
+public sealed record FindUserByIdQuery(int Id) : IRequest<UserDTO>;
 
 internal sealed class FindUserByIdQueryHandler(IAppDbContext context)
-    : IRequestHandler<FindUserByIdQuery, FindUserByIdQueryResponse>
+    : IRequestHandler<FindUserByIdQuery, UserDTO>
 {
     readonly IAppDbContext _context = context;
 
-    public async Task<FindUserByIdQueryResponse> Handle(
+    public async Task<UserDTO> Handle(
         FindUserByIdQuery req,
         CancellationToken cancellationToken)
     {
         var foundUser = await _context
             .Users
-            .FindAsync([req.Id], cancellationToken: cancellationToken) 
+            .Where(u => u.Id == req.Id) 
+            .Select(u => new UserDTO 
+            { 
+                Id = u.Id,
+                UserName = u.Username,
+                Email = u.Email,
+                ConfirmedEmail = u.ConfirmedEmail,
+            })
+            .FirstOrDefaultAsync(cancellationToken)
             ?? throw new UserNotFoundException();
 
-        FindUserByIdQueryResponse res = new (
-            foundUser.Id,
-            foundUser.Email,
-            foundUser.Urls,
-            foundUser.CreatedAt,
-            foundUser.UpdatedAt);
-
-        return res;
+        return foundUser;
     }
 }
---
+++ Src/Infrastructure/Migrations/20240919165223_EmailVerificationEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Shortener.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailVerificationEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfirmedEmail",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "EmailVerifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmailVerifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmailVerifications_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmailVerifications_UserId",
+                table: "EmailVerifications",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmailVerifications");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmedEmail",
+                table: "Users");
+        }
+    }
+}
