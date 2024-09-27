b7cf0ba6f144a4d399f37105fd1dcc69185d9bd3
b_e_sa <joaospereira963@gmail.com>
2024-09-27 12:58:43 -0300

    rename test folders

--- Tests/Application.FunctionalTests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using Shortener.Domain.Entities;
-global using System.Threading.Tasks;
-global using FluentAssertions;
-global using System.Net;
-global using System.Net.Http;
-global using Shortener.Tests.Application.FunctionalTests.Abstractions;
-global using System.Net.Http.Json;
