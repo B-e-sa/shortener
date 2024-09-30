a8a14bf17ee78f670df00bafa9c48f8842548014
b_e_sa <joaospereira963@gmail.com>
2024-09-30 17:18:59 -0300

    remove duplicated Id attribute fom EmailVerification class

--- Src/Domain/Entities/EmailVerification.cs
+++ Src/Domain/Entities/EmailVerification.cs
@@ -5,7 +5,6 @@ namespace Shortener.Domain.Entities
 {
     public class EmailVerification : BaseEntity
     {
-        public int Id { get; set; }
         public string Code { get; set; } = CodeGenerator.Numbers(6);
         public int UserId { get; set; }
         [JsonIgnore]
